fc20709b0b49b94a99ae3ab0c2b7cf400ff02829
Ethan Patterson <eryanpatterson@gmail.com>
2021-11-05 22:19:58 -0400

    Store tokens and delete after one use

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require('jsonwebtoken');
-const { group, verify, address } = require("../lib/mongoose");
+const { group, verify, address, deleteToken } = require("../lib/mongoose");
 require('dotenv').config('../.env.local');
 
 const PORT = process.env.PORT || 3001;
@@ -9,9 +9,10 @@ const app = express();
 app.use(express.json());
 
 app.post("/group-register", async (req, res) => { 
+    console.log(req.body)
     try {
         await group(req.body);
-        res.status(200).send();
+        res.status(200).send({message: "Hello"});
     } catch {
         res.status(400);
     }
@@ -19,20 +20,27 @@ app.post("/group-register", async (req, res) => {
 
 app.post("/verify-email", authenticateToken, async (req, res) => {
     const member = req.member;
-    console.log(member.name);
     try {
         await verify(member);
     } catch {
         res.sendStatus(403)
     }
+    await deleteToken(req.body.token);
     const token = jwt.sign(member, process.env.TOKEN_SECRET);
     res.json({
         token: token
     });
 })
 
-app.post("/address", authenticateToken, async (req, res) => {
-    address(req.member, req.body.address);
+app.post("/address", authenticateToken, async (req, res) => {  
+    
+    try {
+        await address(req.member, req.body.address);
+        res.sendStatus(200);
+    } catch {
+        res.sendStatus(403);
+    }
+
 
 })
 
